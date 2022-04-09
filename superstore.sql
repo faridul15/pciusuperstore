2b6f74ace5ce0d0d370c4a0a00271a0535f044cb
Faridul Hasan <93898161+faridul15@users.noreply.github.com>
2022-04-09 12:55:45 +0600

    Add files via upload

---
+++ superstore.sql
@@ -0,0 +1,323 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2022 at 03:38 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `superstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `ANAME` varchar(20) NOT NULL,
+  `APASS` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`ANAME`, `APASS`) VALUES
+('Sakib', '6983'),
+('Hasib', '6996'),
+('Faridul', '6995');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dstbr`
+--
+
+CREATE TABLE `dstbr` (
+  `DID` int(11) NOT NULL,
+  `DNAME` varchar(20) NOT NULL,
+  `DPASS` varchar(20) NOT NULL DEFAULT 'admin',
+  `DTYPE` varchar(20) NOT NULL,
+  `DLOC` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dstbr`
+--
+
+INSERT INTO `dstbr` (`DID`, `DNAME`, `DPASS`, `DTYPE`, `DLOC`) VALUES
+(1000, 'PRAN', 'admin', 'Beverage', 'Dhaka'),
+(1001, 'Pepsico', 'admin', 'CoolDrinks', 'Chittagong'),
+(1002, 'Unilever', 'admin', 'Consumergoods', 'Hadifokirhat'),
+(1003, 'Walton', 'admin', 'Electonics', 'Chittagong');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `SALESID` int(11) NOT NULL,
+  `SDATE` varchar(20) NOT NULL,
+  `SCOST` int(11) NOT NULL,
+  `SID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`SALESID`, `SDATE`, `SCOST`, `SID`) VALUES
+(222, '22/11/2021', 40079, 100),
+(227, '11/01/2022', 59879, 100),
+(228, '27/11/2021', 60000, 100),
+(229, '25/01/2022', 70000, 100),
+(230, '20/03/2022', 1500, 101),
+(231, '28/03/2022', 2000, 101);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `CRY` varchar(20) NOT NULL,
+  `SCRY` varchar(20) NOT NULL,
+  `QUANT` int(11) NOT NULL,
+  `SID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`CRY`, `SCRY`, `QUANT`, `SID`) VALUES
+('Toothpaste', 'Closeup', 80, 100),
+('Hand Sanitizer', 'Lifeboy', 100, 100),
+('Handwash', 'Lifeboy', 120, 100),
+('Soft Glow Soap', 'Lux', 150, 100),
+('Lotion', 'Vaseline', 70, 100),
+('Toothpaste', 'Closeup', 90, 101),
+('Hand Sanitizer', 'Lifeboy', 100, 101),
+('Handwash', 'Lifeboy', 120, 101),
+('Soft Glow Soap', 'Lux', 130, 101),
+('Lotion', 'Vaseline', 80, 101),
+('Toothpaste', 'Closeup', 95, 102),
+('Hand Sanitizer', 'Lifeboy', 90, 102),
+('Handwash', 'Lifeboy', 110, 102),
+('Soft Glow Soap', 'Lux', 1100, 102),
+('Lotion', 'Vaseline', 60, 102);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+CREATE TABLE `store` (
+  `SID` int(11) NOT NULL,
+  `SPASS` varchar(20) NOT NULL DEFAULT 'admin',
+  `SBRANCHNAME` varchar(20) NOT NULL,
+  `SCITY` varchar(20) NOT NULL,
+  `SREGION` varchar(20) NOT NULL,
+  `SSTATE` varchar(20) NOT NULL,
+  `SPCODE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `store`
+--
+
+INSERT INTO `store` (`SID`, `SPASS`, `SBRANCHNAME`, `SCITY`, `SREGION`, `SSTATE`, `SPCODE`) VALUES
+(100, 'admin', 'GEC Branch', 'Chittagong', 'Chittagong', 'Chittagong', 4000),
+(101, 'admin', 'Halishahar', 'Chittagong', 'Chittagong', 'Bangladesh', 4216),
+(102, 'admin', 'Chawkbazar', 'Chittagong', 'Chittagong', 'Chittagong', 4203);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `strorders`
+--
+
+CREATE TABLE `strorders` (
+  `SID` int(11) NOT NULL,
+  `ORDID` int(11) NOT NULL,
+  `DID` int(11) NOT NULL,
+  `ORDDATE` timestamp NOT NULL DEFAULT current_timestamp(),
+  `PMYSTAT` varchar(20) NOT NULL DEFAULT 'PENDING',
+  `SHPMODE` varchar(20) NOT NULL DEFAULT 'Normal',
+  `SHPSTAT` varchar(20) NOT NULL DEFAULT 'PENDING'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `strorders`
+--
+
+INSERT INTO `strorders` (`SID`, `ORDID`, `DID`, `ORDDATE`, `PMYSTAT`, `SHPMODE`, `SHPSTAT`) VALUES
+(100, 500, 1000, '2018-11-20 20:52:59', '', 'Premium', 'Delivered'),
+(0, 501, 1000, '2018-11-20 21:14:45', 'PAID', 'Normal', 'PENDING'),
+(100, 502, 1000, '2018-11-20 21:15:45', 'PAID', 'Premium', 'Delivered'),
+(100, 503, 1000, '2018-11-20 21:20:11', 'PAID', 'Normal', 'PENDING'),
+(100, 504, 1000, '2018-11-20 21:37:23', '', 'Normal', 'PENDING'),
+(100, 505, 1000, '2018-11-20 21:39:46', '', 'Normal', 'PENDING'),
+(100, 506, 1000, '2018-11-21 13:46:39', '', 'Normal', 'PENDING'),
+(100, 507, 1000, '2018-11-22 09:47:29', '', 'Normal', 'PENDING'),
+(100, 508, 1000, '2018-11-30 15:11:59', '', 'Normal', 'PENDING'),
+(101, 509, 1001, '2022-03-20 09:22:00', 'PENDING', 'Normal', 'PENDING'),
+(101, 510, 1001, '2022-03-28 06:41:40', 'PENDING', 'Normal', 'PENDING');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t`
+--
+
+CREATE TABLE `t` (
+  `temp` varchar(20) NOT NULL,
+  `tee` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t`
+--
+
+INSERT INTO `t` (`temp`, `tee`) VALUES
+('', '2018-11-30 19:21:11'),
+('', '2018-11-30 19:21:32'),
+('501', '2018-11-30 19:22:00'),
+('503', '2018-11-30 19:25:07'),
+('501', '2018-11-30 19:25:20'),
+('502', '2018-11-30 19:25:47'),
+('501', '2018-11-30 19:31:55'),
+('501', '2018-11-30 19:34:22'),
+('501', '2018-11-30 19:34:27'),
+('505', '2018-11-30 19:34:30'),
+('502', '2018-11-30 19:35:50'),
+('506', '2018-11-30 19:35:54'),
+('503', '2018-11-30 19:36:36'),
+('501', '2018-11-30 19:37:40'),
+('501', '2018-11-30 19:38:26'),
+('501', '2018-11-30 19:38:41'),
+('501', '2018-11-30 19:39:42'),
+('502', '2018-11-30 19:47:54'),
+('503', '2018-11-30 19:47:56'),
+('503', '2018-11-30 19:49:10'),
+('505', '2018-11-30 19:49:13'),
+('505', '2018-11-30 19:49:15'),
+('501', '2018-11-30 19:49:17'),
+('503', '2018-11-30 19:50:47'),
+('501', '2018-11-30 19:50:53'),
+('502', '2018-11-30 19:52:55'),
+('502', '2018-11-30 19:53:10'),
+('502', '2018-11-30 19:53:32'),
+('SCITY', '2018-11-30 19:56:55'),
+('SREGION', '2018-11-30 19:58:50'),
+('SBRANCHNAME', '2018-11-30 19:59:08'),
+('SBRANCHNAME', '2018-12-01 04:09:12'),
+('501', '2018-12-01 04:15:09'),
+('SCITY', '2022-03-20 09:04:35'),
+('SSTATE', '2022-03-20 09:16:02'),
+('SCITY', '2022-03-28 08:06:47'),
+('None', '2022-03-28 08:07:18'),
+('None', '2022-03-28 08:07:21'),
+('SBRANCHNAME', '2022-03-28 08:08:36');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dstbr`
+--
+ALTER TABLE `dstbr`
+  ADD PRIMARY KEY (`DID`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`SALESID`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD KEY `SID` (`SID`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
+  ADD PRIMARY KEY (`SID`);
+
+--
+-- Indexes for table `strorders`
+--
+ALTER TABLE `strorders`
+  ADD PRIMARY KEY (`ORDID`),
+  ADD KEY `DID` (`DID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dstbr`
+--
+ALTER TABLE `dstbr`
+  MODIFY `DID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1004;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `SALESID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=232;
+
+--
+-- AUTO_INCREMENT for table `store`
+--
+ALTER TABLE `store`
+  MODIFY `SID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+
+--
+-- AUTO_INCREMENT for table `strorders`
+--
+ALTER TABLE `strorders`
+  MODIFY `ORDID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=511;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`SID`) REFERENCES `store` (`SID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `strorders`
+--
+ALTER TABLE `strorders`
+  ADD CONSTRAINT `strorders_ibfk_1` FOREIGN KEY (`DID`) REFERENCES `dstbr` (`DID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
